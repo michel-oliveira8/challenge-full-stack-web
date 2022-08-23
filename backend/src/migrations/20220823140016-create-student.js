7158a59eb6fd677e4627df558c62c981e5323789
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-08-23 16:35:19 -0300

    feat: migrations

---
+++ backend/src/migrations/20220823140016-create-student.js
@@ -0,0 +1,36 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      cpf: {
+        type: Sequelize.STRING,
+      },
+      RA: {
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Students');
+  },
+};
