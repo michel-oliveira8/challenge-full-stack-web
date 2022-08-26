ff70ee1a02119cd95b88b2bae630c0b4f71b4479
Michel Oliveira <michelsantosoliveira@hotmail.com>
2022-08-26 17:40:16 -0300

    chore: change message(backend)

--- backend/src/schemas/validations.js
+++ backend/src/schemas/validations.js
@@ -7,21 +7,21 @@ const validateName = (name) => {
 const validateEmail = (email) => {
   if (!email || email === '') return { code: 400, message: 'Email is required' };
   const validEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
-  if (!validEmail) return { code: 400, message: 'Email must be a valid email' };
+  if (!validEmail) return { code: 400, message: 'E-mail must be valid' };
 
   return {};
 };
 
 const validateCpf = (cpf) => {
   if (!cpf || cpf === '') return { code: 400, message: 'CPF is required' };
-  if (cpf.length < 11) return { code: 400, message: 'CPF length must be 11 characteres long' };
+  if (!cpf.length === 11) return { code: 400, message: 'CPF length must be 11 characteres long' };
 
   return {};
 };
 
 const validateRA = (ra) => {
   if (!ra) return { code: 400, message: 'RA is required' };
-  if (ra.length < 6) return { code: 400, message: 'RA length must be 6 characteres long' };
+  if (!ra.length === 6) return { code: 400, message: 'RA length must be 6 characteres long' };
 
   return {};
 };
